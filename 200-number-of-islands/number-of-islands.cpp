d1bbdd1db2923d59ed43b5bdc1e18a1ac3246c10
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2024-06-12 01:07:08 +0530

    Time: 31 ms (36.71%) | Memory: 17.9 MB (25.76%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,69 @@
+class DST {
+public:
+    vector<int> size;
+    vector<int> parent;
+
+    DST(int m, int n) {
+        size.resize(m * n, 1);
+        parent.resize(m * n);
+        for (int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUP(int n) {
+        if (parent[n] != n) {
+            parent[n] = findUP(parent[n]); // Correct path compression
+        }
+        return parent[n];
+    }
+
+    void Union(int a, int b) {
+        int pa = findUP(a);
+        int pb = findUP(b);
+        if (pa != pb) { // Only unite if they are in different sets
+            if (size[pa] > size[pb]) {
+                parent[pb] = pa;
+                size[pa] += size[pb];
+            } else {
+                parent[pa] = pb;
+                size[pb] += size[pa];
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        if (m == 0) return 0;
+        int n = grid[0].size();
+        
+        DST ds(m, n);
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    if (i + 1 < m && grid[i + 1][j] == '1') {
+                        ds.Union(i * n + j, (i + 1) * n + j);
+                    }
+                    if (j + 1 < n && grid[i][j + 1] == '1') {
+                        ds.Union(i * n + j, i * n + j + 1);
+                    }
+                }
+            }
+        }
+        
+        unordered_set<int> uniqueIslands;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    uniqueIslands.insert(ds.findUP(i * n + j));
+                }
+            }
+        }
+        
+        return uniqueIslands.size();
+    }
+};
