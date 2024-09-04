a6d36160c5d8823de8229c4d069c45e9e7aae0e2
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2024-09-04 21:08:57 +0530

    Time: 2 ms (63.21%) | Memory: 7.4 MB (74.68%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    int dp[20];
     int catalan_num(int n){
         if(n==0 || n==1)return 1;
-
+        if(dp[n]!=-1)return dp[n];
         int result =0;
         for(int i=0;i<n;i++){
             result+=catalan_num(i)*catalan_num(n-i-1);
         }
-        return result;
+        return dp[n]=result;
     }
     int numTrees(int n) {
         // Structurally unique binary search trees
         // having exactly n nodes 
         // This will be done using catalan num concept
+        memset(dp,-1,sizeof dp);
         return catalan_num(n);
     }
 };
