f11565f345eda6be8bf85e2c7fc8e807cc2ef47f
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-01-07 01:15:33 +0530

    Time: 50 ms (71.21%) | Memory: 48.9 MB (56.51%) - LeetSync

--- 2733-sleep/sleep.js
+++ 2733-sleep/sleep.js
@@ -3,10 +3,14 @@
  * @return {Promise}
  */
 async function sleep(millis) {
+    // callback to be passed into Promise
     function callback(resolve,reject){
+        // needs to be executeed after a given time interval
         setTimeout(resolve,millis)
     }
+    // declaring a promise
     const promise = new Promise(callback)
+    // returning a promise
     return promise;
 
 }
