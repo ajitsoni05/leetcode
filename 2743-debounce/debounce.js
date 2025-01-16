7cc600b9e2c5355737db86a6756da28d0ee364d2
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-01-16 23:03:24 +0530

    Time: 65 ms (23.89%) | Memory: 49.4 MB (48.99%) - LeetSync

---
+++ 2743-debounce/debounce.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let id;
+    return function(...args) {
+        clearTimeout(id);// undefined
+        id = setTimeout(()=>fn(...args),t);
+        return id
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
