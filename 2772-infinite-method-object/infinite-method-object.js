da53628198b15ab2efeb3f4db85ab10ca07e8171
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-02-09 20:36:11 +0530

    Time: 48 ms (76.14%) | Memory: 48.9 MB (39.77%) - LeetSync

---
+++ 2772-infinite-method-object/infinite-method-object.js
@@ -0,0 +1,45 @@
+/**
+ * @return {Object}
+ */
+var createInfiniteObject = function() {
+   /*
+    
+    let targetObject = {
+        attribute : "value"
+    }
+
+    let handler = {
+        // getter
+        get(target,prop,reciever){
+            return "intercepted " + target[prop]
+        }
+    }
+    let p1 = new proxy(targetObject, handler)
+
+    console.log(p1.attribute)
+   
+   */
+
+    let target = {}
+   let proxyObj = new Proxy(target,{
+
+    get(target,prop,reciever){
+        target = {
+            prop: prop,
+        }
+        return function(){
+            return prop;
+        };
+    }
+   })
+
+    return proxyObj
+   
+    
+
+};
+
+/**
+ * const obj = createInfiniteObject();
+ * obj['abc123'](); // "abc123"
+ */
