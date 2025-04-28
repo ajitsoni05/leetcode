fa03381b88d882934525051b33e8fe237fe5cb61
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-04-29 00:36:27 +0530

    Time: 29 ms (45.63%) | Memory: 16.7 MB (61.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,76 +1,38 @@
-class DST{
-    public:
-    vector<int>size;
-    vector<int>parent;
-
-    DST(int m,int n){
-        size.resize(m*n,1);
-        parent.resize(m*n);
-        for(int i=0;i<parent.size();i++){
-            parent[i]=i;
-        }
-    }
-
-   int findUP(int n) {
-        if (parent[n] != n) {
-            parent[n] = findUP(parent[parent[n]]); // Correct path compression
-        }
-        return parent[n];
-    }
-      void Union(int a, int b) {
-        int pa = findUP(a);
-        int pb = findUP(b);
-        if (pa != pb) { // Only unite if they are in different sets
-            if (size[pa] > size[pb]) {
-                parent[pb] = pa;
-                size[pa] += size[pb];
-            } else {
-                parent[pa] = pb;
-                size[pb] += size[pa];
-            }
-        }
-    
-    }
-};
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        // Lets try union find
-        int m=grid.size();
-        int n=grid[0].size();
-        DST ds(m,n);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i+1<m and j<n and grid[i][j]=='1' and grid[i+1][j]=='1'){
-                    ds.Union(i*n+j,(i+1)*n+j);
-                }
-                if(i<m and j+1<n and grid[i][j]=='1' and grid[i][j+1]=='1'){
-                     ds.Union(i*n+j,(i)*n+j+1);
-                }
-                if(i-1>=0 and grid[i][j]=='1' and grid[i-1][j]=='1'){
-                     ds.Union(i*n+j,(i-1)*n+j);
-                }
-                if(j-1>=0 and grid[i][j]=='1' and grid[i][j-1]=='1'){
-                    ds.Union(i*n+j,(i)*n+j-1);
-                }
-            }
-        }
-        map<int,int>mp;
-        int count=0;
-        for(int i=0;i<ds.parent.size();i++){
-            int k=i%n;
-            int h=i/n;
-            if(grid[h][k]=='1'){
-                mp[ds.findUP(i)]++;
-                cout<<ds.findUP(i)<<endl;
+    bool isValid(vector<vector<char>>& g,vector<vector<bool>>& vis,int x,int y){
+        int m = g.size();
+        int n = g[0].size();
+        if(x>=0 and y>=0 and x<m and y<n and !vis[x][y] and g[x][y]=='1')return true;
+        return false;
+    }
+    void dfs(vector<vector<char>>& g,vector<vector<bool>>& vis,int x,int y){
+      vis[x][y]=true;
+      for(int i=-1;i<=1;i++){
+        for(int j=-1;j<=1;j++){
+            if(abs(i)!=abs(j) and isValid(g,vis,x+i,y+j)){
+                dfs(g,vis,x+i,y+j);
             }
 
         }
-        return mp.size();
+      }
     }
-    /*
-        10111
-        10101
-        11101
-    */
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<bool>>vis(m,vector<bool>(n,false));
+
+        int count =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!vis[i][j] and grid[i][j]=='1'){
+                   dfs(grid,vis,i,j);
+                   count++; 
+                }
+            }
+        }
+        return count;
+
+    }
 };
