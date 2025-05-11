d94b15d27cbffc102cb446ca6f4b1f9e0d21cc7f
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-11 14:15:36 +0530

    Time: 8 ms (40.45%) | Memory: 20.1 MB (34.03%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,67 +1,59 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-     /*
+        /*
 
-        0 to numCourses - 1
+           0 to numCourses - 1
 
-        prerequisites [ai, bi]
-     
-     
-     */   
+           prerequisites [ai, bi]
 
 
-    // order
-    vector<int>order;
-     // indegree
-    vector<int>indegree(numCourses,0);
+        */
 
+        // order
+        vector<int> order;
+        // indegree
+        vector<int> indegree(numCourses, 0);
 
-    // make an adjaceny list first
-    unordered_map<int,vector<int>>adj;
+        // make an adjaceny list first
+        unordered_map<int, vector<int>> adj;
 
-     for(int i=0;i<prerequisites.size();i++){
-        int from =prerequisites[i][0];
-        int to = prerequisites[i][1];
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int from = prerequisites[i][0];
+            int to = prerequisites[i][1];
 
-        
-        indegree[to]++;
+            indegree[to]++;
 
-
-        adj[from].push_back(to);
-
-     }
-
-    // put courses with indegree 0 into queue
-    queue<int>nodes;
-
-
-    for(int i=0;i<numCourses;i++){
-        if(indegree[i]==0){
-            nodes.push(i);
+            adj[from].push_back(to);
         }
-    }
 
+        // put courses with indegree 0 into queue
+        queue<int> nodes;
 
-
-    while(!nodes.empty()){
-        // take out the node
-
-        int node = nodes.front();
-        nodes.pop();
-
-        for(auto nb: adj[node]){
-            indegree[nb]--;
-            if(!indegree[nb]){
-                nodes.push(nb);
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                nodes.push(i);
             }
         }
-        order.push_back(node);
-       
-    }
 
-    if(order.size()==numCourses)return true;
-    else return false;
+        while (!nodes.empty()) {
+            // take out the node
 
+            int node = nodes.front();
+            nodes.pop();
+
+            for (auto nb : adj[node]) {
+                indegree[nb]--;
+                if (!indegree[nb]) {
+                    nodes.push(nb);
+                }
+            }
+            order.push_back(node);
+        }
+
+        if (order.size() == numCourses)
+            return true;
+        else
+            return false;
     }
 };
