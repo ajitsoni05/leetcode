07704e682531852d2ae576eec613ccf17204907a
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-11 14:12:18 +0530

    Time: 3 ms (81.08%) | Memory: 20.2 MB (26.40%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+     /*
+
+        0 to numCourses - 1
+
+        prerequisites [ai, bi]
+     
+     
+     */   
+
+
+    // order
+    vector<int>order;
+     // indegree
+    vector<int>indegree(numCourses,0);
+
+
+    // make an adjaceny list first
+    unordered_map<int,vector<int>>adj;
+
+     for(int i=0;i<prerequisites.size();i++){
+        int cToTake = prerequisites[i][1];
+        
+        indegree[cToTake]++;
+
+        int from =prerequisites[i][0];
+        int to = prerequisites[i][1];
+
+        adj[from].push_back(to);
+
+     }
+
+    // put courses with indegree 0 into queue
+    queue<int>nodes;
+
+
+    for(int i=0;i<numCourses;i++){
+        if(indegree[i]==0){
+            nodes.push(i);
+        }
+    }
+
+
+
+    while(!nodes.empty()){
+        // take out the node
+
+        int node = nodes.front();
+        nodes.pop();
+
+        for(auto nb: adj[node]){
+            indegree[nb]--;
+            if(!indegree[nb]){
+                nodes.push(nb);
+            }
+        }
+        order.push_back(node);
+       
+    }
+
+    if(order.size()==numCourses)return true;
+    else return false;
+
+    }
+};
