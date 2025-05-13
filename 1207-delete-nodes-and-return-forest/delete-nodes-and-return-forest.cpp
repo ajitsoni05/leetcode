bf78ee66c70b2af72b7fcec27c9f6646d4d77c4e
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-14 00:29:50 +0530

    Time: 12 ms (82.00%) | Memory: 27.3 MB (28.62%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> saveNodes;
+    unordered_map<int, int> nodesToDelete;
+
+    TreeNode* dfs(TreeNode* root) {
+        if (!root) return nullptr;
+
+        root->left = dfs(root->left);
+        root->right = dfs(root->right);
+
+        if (nodesToDelete.count(root->val)) {
+            if (root->left && !nodesToDelete.count(root->left->val)) {
+                saveNodes.push_back(root->left);
+            }
+            if (root->right && !nodesToDelete.count(root->right->val)) {
+                saveNodes.push_back(root->right);
+            }
+            return nullptr;  // delete this node
+        }
+
+        return root;  // keep this node
+    }
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        for (int val : to_delete) {
+            nodesToDelete[val] = 1;
+        }
+
+        if (root && !nodesToDelete.count(root->val)) {
+            saveNodes.push_back(root);
+        }
+
+        dfs(root);
+
+        return saveNodes;
+    }
+};
