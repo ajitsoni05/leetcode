eaf4d8661270e52fa7a3a2c258863c626078deb3
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-15 00:06:01 +0530

    Time: 95 ms (54.18%) | Memory: 181.2 MB (38.48%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void dfs( vector<vector<int>>&adj,int src,vector<bool>&vis,int& count){
+    void dfs(vector<vector<int>>& adj, int src, vector<bool>& vis, int& count) {
         vis[src] = true;
         count++;
 
-        for(auto nb:adj[src]){
-            if(!vis[nb]){
-                dfs(adj,nb,vis,count);
+        for (auto nb : adj[src]) {
+            if (!vis[nb]) {
+                dfs(adj, nb, vis, count);
             }
         }
     }
@@ -14,12 +14,12 @@ public:
         // size of each components and multiply them in pair and add it up
 
         // create an adjacency list
-        vector<vector<int>>adj(n);
+        vector<vector<int>> adj(n);
 
         // visited array
-        vector<bool>vis(n,false);
+        vector<bool> vis(n, false);
 
-        for(int i = 0; i < edges.size(); i++){
+        for (int i = 0; i < edges.size(); i++) {
 
             int v1 = edges[i][0];
             int v2 = edges[i][1];
@@ -28,25 +28,20 @@ public:
             adj[v2].push_back(v1);
         }
 
-        vector<int>sizes;
+        vector<int> sizes;
 
         long long ans = 0;
         long long tot = 0;
         // dfs
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             int count = 0;
-            if(!vis[i])
-            dfs(adj, i, vis, count);
-            
+            if (!vis[i])
+                dfs(adj, i, vis, count);
 
-            ans += tot*count;
-            tot+=count;
+            ans += tot * count;
+            tot += count;
             sizes.push_back(count);
-
-
-
         }
-
 
         // for(int i = 0; i < sizes.size();i++){
         //      for(int j = 0; j < sizes.size();j++){
@@ -55,8 +50,5 @@ public:
         // }
         // }
         return ans;
-
-
-        
     }
 };
