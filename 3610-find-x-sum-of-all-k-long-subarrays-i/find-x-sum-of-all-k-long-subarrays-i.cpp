90ad8d9fb86ab1accb6baa07ae2e0b13255ab1ba
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-21 22:44:09 +0530

    Time: 37 ms (11.50%) | Memory: 44.5 MB (5.13%) - LeetSync

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        /*
+
+            n nums
+
+            k   and    x
+
+            occurence map of x most frequent
+
+            if freqA == freqB and A > B A is more freq
+
+            sum of resulting array
+
+            if array has less than x distinct then x-sum is sum of the array
+
+            n - k + 1 -> ans[i] = (i..i+k-1)
+
+
+        */
+        vector<int>ans;
+        int n = nums.size();
+
+       for(int i = 0; i < n-k+1; i++){
+
+        int tempX = x;
+        // ans[i] = xsum of i -> i + k-1
+        vector<int>smallArray;
+
+        smallArray.assign(nums.begin()+i,nums.begin()+i+k);
+
+        // find x-sum for smallArray
+
+        unordered_map<int,int>occMp;
+
+        for(int i = 0; i < smallArray.size(); i++){
+            occMp[smallArray[i]]++;
+        }
+
+        //array sum
+        int sum = accumulate(smallArray.begin(),smallArray.end(),0);
+        
+        if(occMp.size() < x){
+            ans.push_back(sum);
+            continue;
+        }
+
+        map<int,set<int,greater<int>>,greater<int>>setMap;
+
+
+        for(auto it:occMp){
+            setMap[it.second].insert(it.first);
+        }
+
+        sum = 0;
+        
+        for(auto setArray:setMap){
+            int occ = setArray.first;
+            for(auto setEle:setArray.second){
+               for(int i = 0; i <occ;i++){
+                    sum+=setEle;
+                    if(tempX==0)break;
+               }
+                tempX--;
+                if(tempX==0)break;
+            }
+            if(tempX==0)break;
+        }
+        ans.push_back(sum);
+
+
+        
+        
+       }
+       return ans;
+    }
+};
