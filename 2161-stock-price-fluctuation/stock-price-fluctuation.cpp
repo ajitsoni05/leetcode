42d94ba0683a5eaba67bb7f875137637c19d516d
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-24 15:42:42 +0530

    Time: 124 ms (51.15%) | Memory: 170.9 MB (80.13%) - LeetSync

--- 2161-stock-price-fluctuation/stock-price-fluctuation.cpp
+++ 2161-stock-price-fluctuation/stock-price-fluctuation.cpp
@@ -1,76 +1,30 @@
 class StockPrice {
+    unordered_map<int, int> timestampPrice;
+    map<int, int> priceCount; // price -> frequency
+    int latestTimestamp = 0;
+
 public:
-    // each record has a timestamp and price of stock
-    // another record appear later with same timestamp
-
-    // for price per ts we can use descending order map
-    map<int,int,greater<int>>mp;
-
-    // max heap
-    priority_queue<pair<int,int>>maxHeap;
-
-    // min heap
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minHeap;
-
-
-    StockPrice() {
-      
-    }
-    
     void update(int timestamp, int price) {
-        // update price at same timestamp
-
-       mp[timestamp] = price;
-
-       maxHeap.push({price,timestamp});
-       minHeap.push({price,timestamp});
-
-        
+        if (timestampPrice.count(timestamp)) {
+            int oldPrice = timestampPrice[timestamp];
+            if (--priceCount[oldPrice] == 0) {
+                priceCount.erase(oldPrice);
+            }
+        }
+        timestampPrice[timestamp] = price;
+        priceCount[price]++;
+        latestTimestamp = max(latestTimestamp, timestamp);
     }
-    
+
     int current() {
-        // price corresponding to the last timestamp
-        return mp.begin()->second;
-        
+        return timestampPrice[latestTimestamp];
     }
-    
+
     int maximum() {
-        // max until now
-        while(!maxHeap.empty()){
-            pair<int,int>top = maxHeap.top();
-            int price = top.first;
-            int ts = top.second;
-
-            if(mp[ts]!=price)maxHeap.pop();
-            else{
-                return price;
-            }
-        }
-        return -1;
-
+        return priceCount.rbegin()->first;
     }
-    
-    int minimum() {
-        // min
-        while(!minHeap.empty()){
-            pair<int,int>top = minHeap.top();
-            int price = top.first;
-            int ts = top.second;
 
-            if(mp[ts]!=price)minHeap.pop();
-            else{
-                return price;
-            }
-        }
-        return -1;
+    int minimum() {
+        return priceCount.begin()->first;
     }
 };
-
-/**
- * Your StockPrice object will be instantiated and called as such:
- * StockPrice* obj = new StockPrice();
- * obj->update(timestamp,price);
- * int param_2 = obj->current();
- * int param_3 = obj->maximum();
- * int param_4 = obj->minimum();
- */
