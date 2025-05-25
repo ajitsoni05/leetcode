48a4ec9abd7d490a463f4d348b0cb3c17a8c0a1d
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-25 14:08:58 +0530

    Time: 18 ms (82.81%) | Memory: 62.6 MB (89.44%) - LeetSync

--- 1582-design-browser-history/design-browser-history.cpp
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -1,47 +1,27 @@
 class BrowserHistory {
 public:
-    deque<string>dq;
-    deque<string>::iterator latest = dq.end();
-    BrowserHistory(string homepage) {
-        dq.push_back(homepage);
-        latest = dq.begin();
-    }
-    
-    void visit(string url) {
-        dq.erase(latest+1,dq.end());
-        dq.push_back(url);
-        latest = dq.end()-1;
+    vector<string> history;
+    int currentIndex;
 
-        
+    BrowserHistory(string homepage) {
+        history.push_back(homepage);
+        currentIndex = 0;
     }
-    
+
+    void visit(string url) {
+        // Drop all forward history
+        history.resize(currentIndex + 1);
+        history.push_back(url);
+        currentIndex++;
+    }
+
     string back(int steps) {
-        
-        while(latest != dq.begin() and steps > 0){
-            latest--;
-            steps--;
-        }
-        return *latest;
-        
+        currentIndex = max(0, currentIndex - steps);
+        return history[currentIndex];
     }
-    
+
     string forward(int steps) {
-        while(latest !=dq.end() and steps >0){
-            latest++;
-            steps--;
-        }
-        if(latest == dq.end()){
-            latest--;
-        }
-        return *latest;
-        
+        currentIndex = min((int)history.size() - 1, currentIndex + steps);
+        return history[currentIndex];
     }
 };
-
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * BrowserHistory* obj = new BrowserHistory(homepage);
- * obj->visit(url);
- * string param_2 = obj->back(steps);
- * string param_3 = obj->forward(steps);
- */
