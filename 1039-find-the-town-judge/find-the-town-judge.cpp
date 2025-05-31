a1f72c23c715033d9c0989569f27e0ada929f9d1
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-05-31 15:33:34 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (81.21%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        // check the indegree and outdegree
-        vector<int>indegree(n+1,0);
-        vector<int>outdegree(n+1,0);
+        vector<int>trustScore(n+1,0);
 
         for(int i = 0; i <trust.size();i++){
             int to = trust[i][1];
             int from = trust[i][0];
 
-            indegree[to]++;
-            outdegree[from]++;
+            trustScore[to]++;
+            trustScore[from]--;
         }
 
 
         for(int i =1; i <=n;i++){
-            if(indegree[i]==n-1 and outdegree[i]==0)return i;
+            if(trustScore[i]==n-1)return i;
         }
+
+        // can be done using single array also
         return -1;
     }
 };
