c1293e8c4692f7615ccce951538390106b99be31
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-02 02:37:05 +0530

    Time: 530 ms (5.01%) | Memory: 21.5 MB (25.68%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+
+
+
+        vector<int>dp(n,INT_MAX);
+
+        dp[0] = 0;
+
+        for(int i = 1; i < n;i++){
+            for(int j = i-1; j>=0;j--){
+                if(nums[j]+j >=i)
+                dp[i] = min(dp[i],(int)(1+dp[j]));
+            }
+        }
+
+        return dp[n-1];
+
+
+
+    }
+};
