ae14b7cce6931f8d0b1fc61e6409f7b0b5e0b2bf
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-02 22:52:13 +0530

    Time: 14 ms (5.13%) | Memory: 36.9 MB (5.13%) - LeetSync

---
+++ 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findDistance(TreeNode* root, int p, int l) {
+        // BFS to populate parent mapping
+        unordered_map<int, int> parent;
+        queue<TreeNode*> q;
+        q.push(root);
+        parent[root->val] = -1;
+
+        bool foundp = false, foundl = false;
+
+        while (!q.empty() && (!foundp || !foundl)) {
+            TreeNode* node = q.front(); q.pop();
+            if (node->val == p) foundp = true;
+            if (node->val == l) foundl = true;
+
+            if (node->left) {
+                parent[node->left->val] = node->val;
+                q.push(node->left);
+            }
+            if (node->right) {
+                parent[node->right->val] = node->val;
+                q.push(node->right);
+            }
+        }
+
+        // Track distance from p to root
+        unordered_map<int, int> distFromP;
+        int curr = p, dist = 0;
+        while (curr != -1) {
+            distFromP[curr] = dist++;
+            curr = parent[curr];
+        }
+
+        // Go from l upward, find LCA
+        curr = l;
+        dist = 0;
+        while (distFromP.find(curr) == distFromP.end()) {
+            curr = parent[curr];
+            dist++;
+        }
+
+        // LCA found at curr
+        return dist + distFromP[curr];
+    }
+};
