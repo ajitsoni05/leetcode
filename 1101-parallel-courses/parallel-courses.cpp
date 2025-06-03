7426485dde46d78c9f4dc8f878c1c0496c6da98c
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-03 21:27:12 +0530

    Time: 8 ms (72.92%) | Memory: 32.5 MB (52.08%) - LeetSync

--- 1101-parallel-courses/parallel-courses.cpp
+++ 1101-parallel-courses/parallel-courses.cpp
@@ -1,38 +1,59 @@
 class Solution {
 public:
     int minimumSemesters(int n, vector<vector<int>>& relations) {
-        vector<int> indegree(n + 1, 0);
-        vector<vector<int>> adj(n + 1);
+        // number of level traversed in the topo sort is min number of sem
+        // Kahn's algo        
 
-        for (const auto& rel : relations) {
-            int u = rel[0], v = rel[1];
-            adj[u].push_back(v);
-            indegree[v]++;
+        // indegree array
+        vector<int>indegree(n+1,0);
+
+        // let's create an adjacency list first
+        vector<vector<int>>adj(n+1);
+
+        // build adj
+
+        for(int i = 0; i < relations.size(); i++){
+            int node1 = relations[i][0];
+            int node2 = relations[i][1];
+
+            indegree[node2]++;
+            adj[node1].push_back(node2);
+        }   
+
+
+       
+
+        // queue 
+        queue<int>q;
+
+
+        for(int i = 1; i < indegree.size();i++){
+            if(indegree[i]==0)q.push(i);
         }
+        
+        int numSem = 0;
+        set<int>visitedSet;
 
-        queue<int> q;
-        for (int i = 1; i <= n; ++i) {
-            if (indegree[i] == 0) q.push(i);
-        }
+        while(!q.empty()){
 
-        int semesters = 0;
-        int visitedCount = 0;
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++){
 
-        while (!q.empty()) {
-            int sz = q.size();
-            while (sz--) {
                 int node = q.front();
                 q.pop();
-                visitedCount++;
-                for (int neighbor : adj[node]) {
-                    if (--indegree[neighbor] == 0) {
-                        q.push(neighbor);
-                    }
+                visitedSet.insert(node);
+                for(auto nb:adj[node]){
+                    if(--indegree[nb]==0)q.push(nb);
+                    
                 }
             }
-            semesters++;
+            numSem++;
+
         }
 
-        return visitedCount == n ? semesters : -1;
+        return numSem and visitedSet.size() == n ? numSem : -1;
+
+        
     }
 };
