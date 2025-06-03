bb76417144865acad44576c135a5e55566c4579c
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-03 18:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (7.43%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -1,17 +1,40 @@
 class Solution {
-public:
-    bool isSame(TreeNode* root1, TreeNode* root2) {
-        if (!root1 && !root2) return true;
-        if (!root1 || !root2) return false;
+   public:
+    // CONSTANTS
+    const int MOD_1 = 1000000007;
+    const int MOD_2 = 2147483647;
 
-        return root1->val == root2->val &&
-               isSame(root1->left, root2->left) &&
-               isSame(root1->right, root2->right);
+    // Hashing a Node
+    pair<unsigned long long, unsigned long long> hashSubtreeAtNode(TreeNode* node, bool needToAdd) {
+        if (node == nullptr) return {3, 7};
+
+        auto left = hashSubtreeAtNode(node->left, needToAdd);
+        auto right = hashSubtreeAtNode(node->right, needToAdd);
+
+        auto left1 = (left.first << 5) % MOD_1;
+        auto right1 = (right.first << 1) % MOD_1;
+        auto left2 = (left.second << 7) % MOD_2;
+        auto right2 = (right.second << 1) % MOD_2;
+
+        pair hashpair = {(left1 + right1 + node->val) % MOD_1,
+                         (left2 + right2 + node->val) % MOD_2};
+
+        if (needToAdd) memo.push_back(hashpair);
+
+        return hashpair;
     }
 
+    // Vector to store hashed value of each node.
+    vector<pair<unsigned long long, unsigned long long>> memo;
+
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (!root) return false;
-        if (isSame(root, subRoot)) return true;
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        // Calling and adding hash to vector
+        hashSubtreeAtNode(root, true);
+
+        // Storing hashed value of subRoot for comparison
+        pair<unsigned long long, unsigned long long> s = hashSubtreeAtNode(subRoot, false);
+
+        // Check if hash of subRoot is present in memo
+        return find(memo.begin(), memo.end(), s) != memo.end();
     }
 };
