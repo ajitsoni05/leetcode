186dab69e92679018698c5b61fa2a7ebb1351449
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-11 02:30:03 +0530

    Time: 3 ms (65.83%) | Memory: 17.5 MB (42.22%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,31 +1,31 @@
-
 class Solution {
-private:
-    unordered_map<int, int> points;
-    unordered_map<int, int> cache;
-
-    int maxPoints(int num) {
-        // Base cases
-        if (num == 0) return 0;
-        if (num == 1) return points.count(1) ? points[1] : 0;
-
-        if (cache.count(num)) return cache[num];
-
-        int gain = points.count(num) ? points[num] : 0;
-        cache[num] = max(maxPoints(num - 1), maxPoints(num - 2) + gain);
-        return cache[num];
-    }
-
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int maxNumber = 0;
 
-        // Precompute total points for each number
-        for (int num : nums) {
-            points[num] += num;
-            maxNumber = max(maxNumber, num);
+        unordered_map<int,int>points;
+        vector<int>maxPoints;
+
+        int maxNum = 0;
+
+        for(auto num:nums){
+            points[num]+= num;
+            maxNum = max(maxNum,num);
         }
 
-        return maxPoints(maxNumber);
+
+        maxPoints.resize(maxNum+1,0);
+
+
+        maxPoints[1] = points.count(1) ? points[1] : 0;
+
+        for(int num = 2; num <= maxNum; num++){
+            maxPoints[num] = max(maxPoints[num-1],points[num] + maxPoints[num-2]);
+        }
+
+        return maxPoints[maxNum];
+
+
+
+
     }
 };
