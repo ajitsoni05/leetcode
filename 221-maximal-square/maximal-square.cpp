c3d7ff75affe6a3ebe56fc20cbed2a0cabd0318f
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-11 20:39:09 +0530

    Time: 12 ms (38.19%) | Memory: 30.8 MB (84.62%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -5,28 +5,22 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<vector<int>>dp(m,vector<int>(n,0));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
 
         int ans = 0;
 
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n;j++){
-                dp[i][j] = matrix[i][j]-'0';
+        for(int i = 1; i <=m; i++){
+            for(int j = 1; j <= n;j++){
+                dp[i][j] = matrix[i-1][j-1]-'0';
+                 ans = max(ans,(dp[i][j]));
+                if(dp[i][j] == 1)
+                dp[i][j] = min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;
                  ans = max(ans,(dp[i][j]));
             }
         }
 
-         for(int i = 1; i < m; i++){
-            for(int j = 1; j < n;j++){
-                ans = max(ans,(dp[i][j]));
 
-                if(dp[i][j] == 1)
-                dp[i][j] = min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;
-
-                ans = max(ans,(dp[i][j]));
-            }
-        }
 
         return ans*ans;
     }
