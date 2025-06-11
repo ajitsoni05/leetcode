1790fac42a50de3c80b0c76e8beb9627926c1e33
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-12 00:57:48 +0530

    Time: 107 ms (42.09%) | Memory: 14.4 MB (47.45%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>lis(n,1);
-        int ans=1;
-        for(int i=1;i<n;i++){
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j]){
-                    lis[i]=max(lis[i],lis[j]+1);
-                    ans=max(ans,lis[i]);
-                }
-            }
+    vector<int>memo;
+    int n;
+    vector<int>numbers;
+    int dp(int i){
+        if(i == n)return 0;
+
+        if(memo[i]!=-1)return memo[i];
+
+        int longest = 1;
+
+        for(int index = i-1; index >= 0; index--){
+           if(numbers[i] > numbers[index]){
+            longest = max(longest,dp(index)+1);
+           }
         }
+
+        return memo[i] = longest;
+
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        // idx LIS at nums.len
+        numbers = nums;
+        n = nums.size();
+        memo.resize(n,-1);
+        
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = max(ans, dp(i));  // compute LIS ending at i
+        }
+
         return ans;
     }
 };
