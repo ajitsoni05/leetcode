681d977c866b184f9c50fbb9173b0dcaa805b033
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-17 23:44:25 +0530

    Time: 2823 ms (5.01%) | Memory: 479.9 MB (5.01%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,80 +1,72 @@
-class dst{
-  public:
-  vector<int>rank;
-  vector<int>parent;
-  int n ;
-  dst(int numNodes):n(numNodes){
-    rank.resize(numNodes,1);
-    for(int i = 0; i < numNodes; i++){
-        parent.push_back(i);
-    }
-  } 
- int find(int x){
-    if(x == parent[x])return x;
-    return parent[x] = find(parent[x]);
- }
- bool un(int x,int y){
-    int rx = find(x);
-    int ry = find(y);
-
-    if(rx != ry){
-        if(rank[rx] > rank[ry]){
-            parent[ry] = rx;
-        }else if(rank[ry] > rank[rx]){
-            parent[rx] = ry;
-        }else{
-            parent[ry] = rx;
-            rank[rx]++;
-        }
-
-        return true;
-    }
-    return false;
- }
-};
 class Solution {
 public:
     int dist(vector<int>p1,vector<int>p2){
         return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1]);
     }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_map<int,pair<int,int>>mp;
+        // lets do using prim's
 
-        int n = points.size();
 
-        vector<vector<int>>edges;
+        // visited set
+        set<int>vis;
 
-        for(int i =0; i < points.size();i++){
-            for(int j = i+1; j <points.size();j++){
-                int distance = dist(points[i],points[j]);
-                edges.push_back({distance,i,j});
-            }
-        }
 
+
+        // min heap
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
 
-        for(int i = 0; i < edges.size();i++){
-            pq.push(edges[i]);
+        int n = points.size();
+        
+        // edges
+        vector<vector<pair<int,int>>>adj(n);
+
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                int node1 = i;
+                int node2 = j;
+
+                int wt = dist(points[i],points[j]);
+                // push the node
+                adj[i].push_back({wt,j});
+                adj[j].push_back({wt,i});
+
+            }
         }
 
-        int iter = 0;
-        dst ds = dst(n);
+
+
+        // start with source
+        pq.push({0,0});
+        
         int cost = 0;
 
-        while(iter != n - 1){
-            vector<int>tp;
-            if(!pq.empty()){
-                tp = pq.top();
-                pq.pop();
-            }
-            int x = tp[1],y =tp[2];
 
-            if(ds.un(x,y)){
-                cost += tp[0];
-                iter++;
+        while(!pq.empty()){
+
+            vector<int> v = pq.top();
+            int wt = v[0];
+            int node = v[1];
+            pq.pop();
+
+
+
+            if(vis.count(node))continue;
+
+            vis.insert(node);
+
+            cost += wt;
+
+            for(auto nb:adj[node]){
+                if(!vis.count(nb.second)){
+                    pq.push({nb.first,nb.second});
+                }
             }
-            
         }
+
+
         return cost;
+
     }
 };
