7da93b5c96296e83988f71dc40f96964ba073a8e
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-20 01:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (74.63%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -3,7 +3,7 @@ public:
     unordered_map<char, int> charMap;
 
     string customSortString(string order, string s) {
-     sort(s.begin(),s.end(),[order](char a,char b){
+     sort(s.begin(),s.end(),[&order](char a,char b){
         return order.find(a) < order.find(b);
      });
 
