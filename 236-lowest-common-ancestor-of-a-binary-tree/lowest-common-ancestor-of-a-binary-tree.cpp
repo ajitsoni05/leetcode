2f28bfb525e4b66f57b8308f18dd5b1d19732e61
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-06-25 23:47:14 +0530

    Time: 20 ms (6.89%) | Memory: 20.7 MB (5.31%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,12 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q) return root;
+        // dfs
+        if(!root) return root;
+        if(root == p)return p;
+        if(root == q)return q;
 
-        TreeNode* l = lowestCommonAncestor(root->left, p, q);
-        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        // lets make parent map
+        unordered_map<TreeNode*,TreeNode*>parent;
+        parent[root] = nullptr;
 
-        if (l && r) return root;  // p and q found in different subtrees
-        return l ? l : r;         // return non-null subtree
+        // queue for traversal
+        queue<TreeNode*>qNodes;
+        
+        // push root
+        qNodes.push(root);
+
+        while(!parent.count(p) || !parent.count(q)){
+            TreeNode* node = qNodes.front();
+            qNodes.pop();
+
+            if(node->left){
+                parent[node->left]= node;
+                qNodes.push(node->left);
+            }
+
+            if(node->right){
+                parent[node->right]= node;
+                qNodes.push(node->right);
+            }
+
+        }
+
+        // gather ancestors
+        unordered_set<TreeNode*>ancestors;
+
+        while(p){
+            ancestors.insert(p);
+            p = parent[p];
+        }
+
+        while(ancestors.find(q) == ancestors.end()){
+            q = parent[q];
+        }
+
+        return q;
     }
 };
