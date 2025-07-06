6489d02b8ff50f19cc06f3b85ddac9f41baba9b9
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-07-06 17:28:24 +0530

    Time: 3 ms (21.08%) | Memory: 47.7 MB (5.37%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,26 +1,70 @@
 class Solution {
 public:
-    int maxSubarraySumCircular(vector<int>& nums) {
+    int maxNormalSum(vector<int>& nums) {
         int curMax = 0;
-        int curMin = 0;
         int maxSum = nums[0];
-        int minSum = nums[0];
 
-        int totalSum = 0;
+        int n = nums.size();
 
-        int n = nums.size()
-;
-        for(int i = 0; i < n; i++){
-            curMax = max(nums[i],curMax+ nums[i]);
-            maxSum = max(maxSum,curMax);
-            curMin = min(nums[i],curMin + nums[i]);
-            minSum = min(minSum,curMin);
-            totalSum += nums[i];
+        for (int i = 0; i < n; i++) {
+            curMax = max(nums[i], curMax + nums[i]);
+
+            maxSum = max(maxSum, curMax);
         }
 
+        return maxSum;
+    }
+    int specialSum(vector<int>& nums) {
+        int n = nums.size();
 
-        if(totalSum == minSum)return maxSum;
-        else return max(maxSum,(totalSum - minSum));
-        
+        vector<int> prefixSum(n, 0);
+
+        int curSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            curSum += nums[i];
+            prefixSum[i] = curSum;
+        }
+
+        vector<int> suffixSum(n, 0);
+
+        curSum = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            curSum += nums[i];
+            suffixSum[i] = curSum;
+        }
+
+        vector<int> rightMax(n, INT_MIN);
+        int rightMaxSum = suffixSum[n - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            rightMaxSum = max(rightMaxSum, suffixSum[i]);
+            rightMax[i] = rightMaxSum;
+        }
+
+        int maxSpecialSum = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            maxSpecialSum = max(maxSpecialSum, prefixSum[i] + rightMax[i + 1]);
+        }
+
+        return maxSpecialSum;
+    }
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int normalMax = maxNormalSum(nums);
+
+
+        bool allNegative = true;
+        for (int num : nums) {
+            if (num > 0) {
+                allNegative = false;
+                break;
+            }
+        }
+        if (allNegative)
+            return normalMax;
+
+        return max(normalMax, specialSum(nums));
     }
 };
