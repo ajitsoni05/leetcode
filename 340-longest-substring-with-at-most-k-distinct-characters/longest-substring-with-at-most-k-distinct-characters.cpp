cf439896a19cd96372eb91588bd5b17c73a76dd0
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2025-07-13 01:53:55 +0530

    Time: 7 ms (59.38%) | Memory: 10.6 MB (63.45%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.cpp
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
+    int getLeastLastIndex(unordered_map<char,int>& lastIndex) {
+        int minm = INT_MAX;
+        for (auto& ele : lastIndex) {
+            minm = min(minm, ele.second);
+        }
+        return minm;
+    }
+
     int lengthOfLongestSubstringKDistinct(string s, int k) {
+        if (k == 0) return 0;
+
+        int l = 0, r = 0;
         int n = s.size();
-
-        if(n*k == 0)return 0;
-
-        int left = 0, right = 0;
-
-        unordered_map<char,int>rightMostIndex;
-
+        unordered_map<char, int> lastIndex;
         int maxLen = 0;
 
-        while(right < n){
-            rightMostIndex[s[right]]++;
-
-            while(rightMostIndex.size() > k){
-                rightMostIndex[s[left]]--;
-                if(rightMostIndex[s[left]] == 0)rightMostIndex.erase(s[left]);
-                left++;
+        while (r < n) {
+            char ch = s[r];
+            if (lastIndex.count(ch)) {
+                lastIndex[ch] = r;
+            } else if (lastIndex.size() < k) {
+                lastIndex[ch] = r;
+            } else {
+                int leastIndex = getLeastLastIndex(lastIndex);
+                char toRemove = s[leastIndex];
+                lastIndex.erase(toRemove);
+                l = leastIndex + 1;
+                lastIndex[ch] = r;
             }
 
-            maxLen = max(maxLen, right - left + 1);
-            right++;
+            maxLen = max(maxLen, r - l + 1);
+            r++;
         }
 
         return maxLen;
