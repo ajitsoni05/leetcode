9d97750e68b021d1ae117d5519890dd6dd31ae81
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-04-08 00:15:48 +0530

    Time: 3 ms (87.29%) | Memory: 12.9 MB (91.10%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -4,18 +4,35 @@ public:
         /*
         */
 
-        int len = s.size();
+        // int len = s.size();
+        // int count = 0;
+        // unordered_map<int,int> tracker;
+
+
+        // tracker[s[0]]++;
+        
+        // for(int i = 1; i < len; i++){
+        //     if(tracker.count(s[i])){
+        //         tracker.clear();
+        //         count++;
+        //     }
+        //     tracker[s[i]]++;
+        // }
+        // return count+1;
+
+        // alternatively  we can do it using lastSeenArray
+
+        vector<int>lastSeen(26,-1);
+        int currStartingIndex = 0;
         int count = 0;
-        unordered_map<int,int> tracker;
 
-
-        tracker[s[0]]++;
-        for(int i = 1; i < len; i++){
-            if(tracker.count(s[i])){
-                tracker.clear();
+        for(int i = 0; i < s.size(); i++){
+            if(lastSeen[s[i] - 'a'] >= currStartingIndex){
                 count++;
+                
+                currStartingIndex = i;
             }
-            tracker[s[i]]++;
+            lastSeen[s[i] - 'a'] = i;
         }
         return count+1;
         
