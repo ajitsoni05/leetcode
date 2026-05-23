c95af87c35a72d2658e3270ea210172f8ef26d3a
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-05-23 17:36:09 +0530

    Time: 4 ms (31.44%) | Memory: 63.1 MB (43.75%) - LeetSync

--- 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.js
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.js
@@ -8,22 +8,36 @@ var findDegrees = function(matrix) {
 
 
     // consider elements on upper diagnoal 
+
+    // forEach has function call over head , 
     
-    matrix.forEach((row, rowIdx) => {
-        row.forEach((val, colIdx)=>{
-            if(rowIdx > colIdx){
-            let isConnected = val;
-            
+    // matrix.forEach((row, rowIdx) => {
+    //     row.forEach((val, colIdx)=>{
+    //         if(rowIdx > colIdx) {
 
-            if(val) {
-                degrees[rowIdx]++;
-                degrees[colIdx]++;
-            }
-            }
-        })
-    })
+    //             if(val) {
+    //                 degrees[rowIdx]++;
+    //                 degrees[colIdx]++;
+    //             }
+    //         }
+    //     })
+    // })
+    
 
+    // if we only see upper triangle (colIdx > rowIdx) or lower triangle (rowIdx > colIdx) then we don't need to halve each value like below
     // for(let i = 0; i < degrees.length; i++) degrees[i] = degrees[i] / 2;
+
+
+    // using for loop is more fast in JS
+    for(let r = 0; r < matrix.length; r++){
+        for(let c = 0; c < matrix[r].length; c++){
+            if (r > c || !matrix[r][c]) continue;
+
+            degrees[r]++;
+            degrees[c]++;
+            
+        }
+    }
 
     return degrees;
 };
