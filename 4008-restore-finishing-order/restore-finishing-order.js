f160cb34f8abad7803daa69e9fd928e8fcc4ca1c
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-05-26 00:24:04 +0530

    Time: 4 ms (5.45%) | Memory: 57 MB (52.24%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.js
+++ 4008-restore-finishing-order/restore-finishing-order.js
@@ -5,14 +5,19 @@
  */
 var recoverOrder = function(order, friends) {
     let friendsSet = new Set();
-    friends.forEach((ele)=>{
+    friends.forEach((ele) => {
         friendsSet.add(ele);
     })
 
+    // replace forEach with for to avoid function call overhead
+    for(let i = 0; i < friends.length; i++) {
+         friendsSet.add(friends[i]);
+    }
+
     let friendsOrder = []
 
-    order.forEach((ele) => {
-        if(friendsSet.has(ele))friendsOrder.push(ele);
-    })
+    for(let i = 0; i < order.length; i++){
+        if(friendsSet.has(order[i]))friendsOrder.push(order[i]);
+    }
     return friendsOrder;
 };
