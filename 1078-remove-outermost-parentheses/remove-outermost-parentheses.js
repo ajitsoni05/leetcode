b20b813d2a309a66752c432574dbab78c243886a
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-05-31 00:37:06 +0530

    Time: 8 ms (7.69%) | Memory: 57.5 MB (31.05%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -7,18 +7,31 @@ var removeOuterParentheses = function (s) {
     let stack = []
     let final = ""
 
-  Array.from(s).forEach((ele) => {
+    // Array.from(s).forEach((ele) => {
 
-        if(ele === "(") {
-            if(stack.length > 0) final += "("
-            stack.push(ele);
-        } else {
+    //     if (ele === "(") {
+    //         if (stack.length > 0) final += "("
+    //         stack.push(ele);
+    //     } else {
+    //         stack.pop();
+    //         if (stack.length > 0)
+    //             final += ")"
+
+
+    //     }
+    // })
+
+    // let's use for loop
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            final += stack.length >= 1 ? "(" : ""
+            stack.push("(");
+
+        }else{
             stack.pop();
-            if(stack.length > 0)
-                final += ")"
-            
+            final += stack.length >= 1 ? ")" : ""
 
         }
-    })
+    }
     return final;
 };
