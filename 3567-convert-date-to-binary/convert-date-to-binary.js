0dd10ce2d6dd47da9a3405bab99ecc164afc2d39
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-05-30 18:53:45 +0530

    Time: 1 ms (58.10%) | Memory: 54.9 MB (79.05%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.js
+++ 3567-convert-date-to-binary/convert-date-to-binary.js
@@ -13,13 +13,15 @@ var convertDateToBinary = function(date) {
         let num = Number(ele)
         let bitStr = ""
 
-        for (let i = 0; i < 32; i++) {
-            let bit = num & (1<<i);
-            if(bit)
-            bitStr = "1" + bitStr
-            else bitStr ="0" + bitStr
-        }
-        finalAns += bitStr.replace(/^0+/, '')  + "-"
+        // for (let i = 0; i < 32; i++) {
+        //     let bit = num & (1<<i);
+        //     if(bit)
+        //     bitStr = "1" + bitStr
+        //     else bitStr ="0" + bitStr
+        // }
+
+        // finalAns += bitStr.replace(/^0+/, '')  + "-"
+        finalAns += num.toString(2) + "-"
 
     })
 
