4485b351c204b53e8e0bfe2bed36f926c4ce768c
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-05-31 00:11:00 +0530

    Time: 68 ms (27.27%) | Memory: 77.9 MB (72.73%) - LeetSync

---
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxWidthOfVerticalArea = function(points) {
+    points.sort((a,b) => a[0] - b[0]);
+
+    let widest = 0;
+
+    for(let i = 0; i < points.length - 1; i++){
+        widest = Math.max(widest, points[i+1][0] - points[i][0])
+    }
+    
+    return widest;
+};
