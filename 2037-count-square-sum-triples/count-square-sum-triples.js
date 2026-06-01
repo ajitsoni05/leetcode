09494d0641d4ded993e2d918dbb8b19346e6e408
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-06-01 23:38:13 +0530

    Time: 61 ms (16.13%) | Memory: 54.7 MB (45.97%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function (n) {
+
+    let count = 0
+    for (let i = 1; i <= n - 2; i++) {
+        for (let j = i + 1; j <= n - 1; j++) {
+            for (let k = j + 1; k <= n; k++) {
+                if(i*i + j*j === k*k)count++;
+            }
+        }
+    }
+    return count*2
+};
