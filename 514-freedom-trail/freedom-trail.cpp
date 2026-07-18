3e251da01a5b71c2c73e14def6f3fe447bfb5df6
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-07-18 22:45:19 +0530

    Time: 24 ms (36.47%) | Memory: 15.6 MB (62.48%) - LeetSync

---
+++ 514-freedom-trail/freedom-trail.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string ring, key;
+    unordered_map<char, vector<int>> rIMap;
+    int kLen, rLen;
+
+    struct PairHash {
+        size_t operator()(const pair<int, int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
+
+    unordered_map<pair<int, int>, int, PairHash> mem;
+
+    int recurse(int kIdx, int currIdx) {
+        if (kIdx == kLen)
+            return 0;
+
+        if (mem.count({kIdx, currIdx}))
+            return mem[{kIdx, currIdx}];
+
+        char c = key[kIdx];
+        int ans = INT_MAX;
+
+        for (int idx : rIMap[c]) {
+            int diff = abs(currIdx - idx);
+            int rotate = min(diff, rLen - diff);
+
+            ans = min(ans,rotate + 1 + recurse(kIdx + 1, idx));
+        }
+
+        return mem[{kIdx, currIdx}] = ans;
+    }
+
+    int findRotateSteps(string ring, string key) {
+        this->ring = ring;
+        this->key = key;
+        rLen = ring.size();
+        kLen = key.size();
+
+        for (int i = 0; i < rLen; i++) {
+            rIMap[ring[i]].push_back(i);
+        }
+
+        return recurse(0, 0);
+    }
+};
