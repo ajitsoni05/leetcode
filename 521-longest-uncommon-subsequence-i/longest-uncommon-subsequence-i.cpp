1fa914d3ace8ec2fd6d1d65eb93705ae29efbc8f
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-07-21 00:18:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (35.04%) - LeetSync

--- 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.cpp
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     int findLUSlength(string a, string b) {
-        
-        /*
-        
-         abc
-         def
-
-         abc
-
-
-         abc
-
-         abc
-
-
-        
-        */
         if(a != b)return a.size() > b.size() ? a.size() : b.size();
         return -1;
     }
