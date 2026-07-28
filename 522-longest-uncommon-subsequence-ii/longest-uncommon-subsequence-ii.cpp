b4f92ec50dc1d65bd48c16623393090b80f3b3a5
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-07-28 06:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (46.09%) - LeetSync

---
+++ 522-longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string a){
+        if(s.size() > a.size())return false;
+
+        int ptrA = 0;
+        int ptrS = 0;
+
+        while(ptrA < a.size()){
+            if(s[ptrS] == a[ptrA]){
+                ptrS++;
+                ptrA++;
+            }else{
+                ptrA++;
+            }
+        }
+
+        if(ptrS == s.size())return true;
+
+        return false;
+
+    }
+    int findLUSlength(vector<string>& strs) {
+        // check full string, if is prefix
+        int n = strs.size();
+        int minLen = 0;
+
+        for(int i = 0; i < n; i++) {
+            bool isCand = true;
+            for(int j = 0; j < n; j++){
+                if(i == j)continue;
+                if(isSubsequence(strs[i], strs[j])){
+                    isCand = false;
+                    break;
+                }
+
+            }
+            if(isCand) {
+                minLen = max(minLen, (int)strs[i].size());
+            }
+        }
+        return minLen ? minLen : -1;
+    }
+};
