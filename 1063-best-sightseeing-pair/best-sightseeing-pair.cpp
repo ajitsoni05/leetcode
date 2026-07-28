1443dee3fc34a40fb2939c9a47ebaa4783ea5b60
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-07-28 08:37:14 +0530

    Time: 7 ms (9.74%) | Memory: 47.3 MB (24.41%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
         /*
-        
-        
-            8   1   5   2   6
-            0   1   2   3   4
-
-            8 + 5 + 0 - 2
-        
+            we can fix a right and use it as values[r] - r
+            and for that right we can have lefts and add it to right as values[l] + l
+            we can have a running max left 
         */
 
-
-        int result = 0;
-        int current_max = values[0] - 1;
         int n = values.size();
+        vector<int>maxLeft(n,0);
+        
 
+        // maxLeft at 0 is same 
+        maxLeft[0] = values[0] - 0;
+
+        // store max score
+        int maxScore = 0;
+        
         for(int i = 1; i < n; i++){
-            result = max(result , current_max + values[i]);
-            
-            current_max = max(current_max, values[i]) - 1;
+            int currentRightScore = values[i] - i;
+            maxScore = max(maxScore, maxLeft[i - 1] + currentRightScore);
+
+            // update maxLeft for i
+            maxLeft[i] = max(maxLeft[i - 1], values[i] + i);
+
         }
-      
-      return result;
+        return maxScore;
 
     }
 };
