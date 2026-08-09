24290c17aa81ac8da05ea4f8869514553de0bb7f
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-08-09 17:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.40%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
-
+        // consider two pointers - reader_ptr and writer_ptr
         /*
-        
-        i 
-        2 2 3 3
-          j
+            reader_ptr reads for val
+            and writer_ptr writes if not val
         
         */
 
+        int reader_ptr = 0, writer_ptr = 0;
         int n = nums.size();
 
-        int back_ptr = n - 1;
-        int front_ptr = 0;
-        int count = 0;
-        
-        // double pass first modify 
-        while(front_ptr < back_ptr){
-            while(back_ptr > 0 and nums[back_ptr] == val)back_ptr--;
-
-            if(front_ptr < back_ptr and nums[front_ptr] == val){
-                swap(nums[front_ptr],nums[back_ptr]);
-                front_ptr++;
-                back_ptr--;
-            }else{
-                front_ptr++;
-            }
+        while(reader_ptr < n){
+            if(nums[reader_ptr] == val)reader_ptr++;
+            else nums[writer_ptr++] = nums[reader_ptr++];
         }
-
-        for(int i = 0; i < nums.size(); i++){
-           if(nums[i] == val) count++;
-        }
-
-        
-        // then count
-       return n - count;
+        return writer_ptr;
     }
 };
