b8d37edb6def4af045de8f6bd6fdf97865a5af06
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-08-09 16:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (11.71%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         // first write nums2 into later part ( > m) of nums1
+        
+        /*
+        
+        Simplest solution
 
         for(int i = 0; i < n; i++){
             nums1[i+m] = nums2[i];
@@ -10,6 +14,34 @@ public:
         // now sort
         sort(nums1.begin(),nums1.end());
         return;
+
+        */
+
+
+        
+        // auxiliary space
+
+        vector<int> merged(m+n, 0);
+
+        int i = 0,j = 0;
+        int w = 0;
+
+        while(i < m || j < n) {
+
+            if(i < m and j < n)
+            merged[w++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+            else if(i < m)
+            merged[w++] = nums1[i++];
+            else
+            merged[w++] = nums2[j++];
+        }
+        
+        // rewrite merged into nums1
+        for(int i = 0; i < m+n; i++){
+            nums1[i] = merged[i];
+        }
+        
+
 
     }
 };
