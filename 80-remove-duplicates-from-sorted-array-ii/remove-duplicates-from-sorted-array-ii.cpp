584ef1f0699c4a10cf179ee699d4d62a78b54c9b
Ajit Soni <49568689+ajitsoni05@users.noreply.github.com>
2026-08-09 17:55:36 +0530

    Time: 4 ms (84.18%) | Memory: 19.6 MB (24.45%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // read once write at most twice
 
-        int i = 0, j = 0;
+        /*
+        
+              r
+            1 1 1 2 2 3
+              w
+
+            counter = 1
+
+        */
+        int reader_ptr = 1;
+        int writer_ptr = 0;
+        int counter = 1;
         int n = nums.size();
 
-        while (j < n) {
-
-            int freq = 0;
-            int val = nums[j];
-
-            // count frequency safely
-            while (j < n && nums[j] == val) {
-                freq++;
-                j++;
-            }
-
-            freq = min(freq, 2);
-
-            // write allowed duplicates
-            while (freq--) {
-                nums[i++] = val;
+        while(reader_ptr < n) {
+            if(nums[reader_ptr] == nums[writer_ptr]){
+                counter++;
+                if(counter <= 2){
+                    nums[++writer_ptr] = nums[reader_ptr];
+                    reader_ptr++;
+                }else{
+                    reader_ptr++;
+                }
+                
+            }else{
+                counter = 1;
+                nums[++writer_ptr] = nums[reader_ptr];
+                reader_ptr++;
             }
         }
-
-        return i;
+        return writer_ptr+1;
     }
 };
